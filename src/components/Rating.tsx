50732fa78afd3fb74feb7a0827d6122baed492d8
dinhhieu1901 <dinhhieu.nguyen@nanofin.tech>
2023-04-25 18:04:10 +0700

    feat: add library lottie

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,14 +1,17 @@
 import { Sad, Smile, Cry, Happy } from "../assets/image";
 import Icon from "./Icon";
+import Lottie from "lottie-react";
+import sadAnimation from "../assets/animation/sad.json";
 
 const Rating = () => {
   return (
     <>
-      <div className="h-[50%] flex justify-center gap-6  bg-[#FFFFFF]">
+      <div className="h-[50%] flex justify-center gap-6 bg-[#FFFFFF]">
         <Icon icon={Sad} label="Buồn" />
         <Icon icon={Smile} label="Haha" />
         <Icon icon={Cry} label="Khóc" />
         <Icon icon={Happy} label="Thương thương" />
+        <Lottie animationData={sadAnimation} label="Buồn" />
       </div>
     </>
   );
