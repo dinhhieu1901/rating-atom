45283ded934721c10a5f52ed0bd381d1ffe332dd
dinhhieu1901 <dinhhieu.nguyen@nanofin.tech>
2023-04-25 10:35:59 +0700

    build: create project

---
+++ src/assets/image/index.ts
@@ -0,0 +1,4 @@
+export { default as Happy } from "../image/icon1.png";
+export { default as Smile } from "../image/icon2.png";
+export { default as Sad } from "../image/icon3.png";
+export { default as Cry } from "../image/icon4.png";
