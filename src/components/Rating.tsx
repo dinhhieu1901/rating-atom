90eb8a35975c6a4b09b2ec8a18f3a73c8d344f89
dinhhieu1901 <dinhhieu.nguyen@nanofin.tech>
2023-04-26 11:21:17 +0700

    chore: change animation library

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,17 +1,21 @@
-import { Sad, Smile, Cry, Happy } from "../assets/image";
-import Icon from "./Icon";
 import Lottie from "lottie-react";
 import sadAnimation from "../assets/animation/sad.json";
+import wowAnimation from "../assets/animation/wow.json";
+import loveAnimation from "../assets/animation/love.json";
 
 const Rating = () => {
   return (
     <>
-      <div className="h-[50%] flex justify-center gap-6 bg-[#FFFFFF]">
-        <Icon icon={Sad} label="Buồn" />
-        <Icon icon={Smile} label="Haha" />
-        <Icon icon={Cry} label="Khóc" />
-        <Icon icon={Happy} label="Thương thương" />
-        <Lottie animationData={sadAnimation} label="Buồn" />
+      <div className="h-[50%] flex justify-center items-center gap-6 bg-[#FFFFFF]">
+        <div className="w-40 h-40 flex rounded-[50%] bg-primary">
+          <Lottie animationData={sadAnimation} />
+        </div>
+        <div className="w-40 h-40 flex rounded-[50%] bg-primary">
+          <Lottie animationData={wowAnimation} />
+        </div>
+        <div className="w-40 h-40 flex rounded-[50%] bg-primary">
+          <Lottie animationData={loveAnimation} loop={2} />
+        </div>
       </div>
     </>
   );
